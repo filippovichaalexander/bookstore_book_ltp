af600e3944a27e3f0e0349c73218e8732b8f84c3
Филиппович Александр Александрович <filippovich.aa@corp.ertelecom.loc>
2024-08-16 15:10:56 +0300

    tessting success backend 1

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -4,31 +4,31 @@ import { Book } from '../models/bookModel.js';
 const router = express.Router();
 
 // Route for Save a new Book
-// router.post('/', async (request, response) => {
-//   try {
-//     if (
-//       !request.body.title ||
-//       !request.body.author ||
-//       !request.body.publishYear
-//     ) {
-//       return response.status(400).send({
-//         message: 'Send all required fields: title, author, publishYear',
-//       });
-//     }
-//     const newBook = {
-//       title: request.body.title,
-//       author: request.body.author,
-//       publishYear: request.body.publishYear,
-//     };
+router.post('/', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
 
-//     const book = await Book.create(newBook);
+    const book = await Book.create(newBook);
 
-//     return response.status(201).send(book);
-//   } catch (error) {
-//     console.log(error.message);
-//     response.status(500).send({ message: error.message });
-//   }
-// });
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 // Route for Get All Books from database
 router.get('/', async (request, response) => {
