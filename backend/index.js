058ca00bb35ae707dae195c514d552e4dfc0ebfd
Филиппович Александр Александрович <filippovich.aa@corp.ertelecom.loc>
2024-08-16 15:09:48 +0300

    testing backend routes

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,10 @@ app.use(cors());
 //   })
 // );
 
-app.get('/', (request, response) => {
-  console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
-});
+// app.get('/', (request, response) => {
+//   console.log(request);
+//   return response.status(234).send('Welcome To MERN Stack Tutorial');
+// });
 
 app.use('/books', booksRoute);
 
